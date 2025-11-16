2675f26fb002127dd4eecb38662a88b6bcb4c75c
Yoga Rizky Alkautsar <yogarizkyalkautsar@Yogas-MacBook-Air.local>
2025-11-16 20:44:43 +0800

    Initial commit portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
