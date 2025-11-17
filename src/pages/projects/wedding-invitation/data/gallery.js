ffb461c3a8768b0feb18ecb2acb1ea7511dc0467
Yoga Rizky Alkautsar <yogaralkautsar@gmail.com>
2025-11-17 16:05:35 +0800

    new projects - wedding invitation

---
+++ src/pages/projects/wedding-invitation/data/gallery.js
@@ -0,0 +1,6 @@
+export const gallery = [
+  "/projects/wedding-invitation/gallery-1.jpg",
+  "/projects/wedding-invitation/gallery-2.jpg",
+  "/projects/wedding-invitation/gallery-3.jpg",
+  "/projects/wedding-invitation/gallery-4.jpg"
+];
