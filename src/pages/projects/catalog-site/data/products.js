8a13c22634af60c17c077ca7ea1b252c70b2d6e2
Yoga Rizky Alkautsar <yogaralkautsar@gmail.com>
2025-11-17 15:43:41 +0800

    new projects - catalog

---
+++ src/pages/projects/catalog-site/data/products.js
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: 1,
+    name: "Iphone Air",
+    price: "Rp 18.000.000",
+    description: "Storage 256 GB.",
+    image: "/projects/catalog-site/product-1.jpg"
+  },
+  {
+    id: 2,
+    name: "Iphone 17",
+    price: "Rp 15.000.000",
+    description: "Storage 512 GB.",
+    image: "/projects/catalog-site/product-2.jpg"
+  },
+  {
+    id: 3,
+    name: "Iphone 17 Pro",
+    price: "Rp 23.000.000",
+    description: "Storage 512 GB.",
+    image: "/projects/catalog-site/product-3.jpg"
+  }
+];
